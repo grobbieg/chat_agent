aad6d08efaa779afa959d71bae737a384a14da23
grobbieg <rob.gibbon@enterprisedb.com>
2025-03-20 20:19:18 -0700

    Update structure.py

--- structure.py
+++ structure.py
@@ -5,6 +5,7 @@ from griptape.configs import Defaults
 from griptape.drivers.memory.conversation.griptape_cloud import GriptapeCloudConversationMemoryDriver
 from griptape.drivers.ruleset.griptape_cloud import GriptapeCloudRulesetDriver
 from griptape.drivers.vector.griptape_cloud import GriptapeCloudVectorStoreDriver
+from griptape.drivers.prompt.griptape_cloud import GriptapeCloudPromptDriver
 from griptape.engines.rag import RagEngine
 from griptape.engines.rag.modules import (
     PromptResponseRagModule,
