b3af4a42b0d6a39bf4864f4b7d5dce2035478e04
grobbieg <rob.gibbon@enterprisedb.com>
2025-06-10 06:31:25 +0200

    Update structure.py

--- structure.py
+++ structure.py
@@ -1,5 +1,5 @@
 import argparse
-import os
+import requests, os, urllib3, ssl
 from dotenv import load_dotenv
 from griptape.configs import Defaults
 from griptape.structures import Pipeline
@@ -8,6 +8,7 @@ from griptape.drivers.memory.conversation.griptape_cloud import GriptapeCloudCon
 from griptape.drivers.ruleset.griptape_cloud import GriptapeCloudRulesetDriver
 from griptape.drivers.vector.griptape_cloud import GriptapeCloudVectorStoreDriver
 from griptape.drivers.assistant.griptape_cloud import GriptapeCloudAssistantDriver
+from griptape.drivers.prompt.griptape_cloud import GriptapeCloudPromptDriver
 from griptape.engines.rag import RagEngine
 from griptape.engines.rag.modules import (
     PromptResponseRagModule,
@@ -18,8 +19,12 @@ from griptape.rules.ruleset import Ruleset
 from griptape.structures import Agent
 from griptape.tools import BaseTool, RagTool
 from griptape.utils import GriptapeCloudStructure
+from pydantic import BaseModel
 
 load_dotenv()
+
+class Output(BaseModel):
+    comprehensive_answer: str
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -57,13 +62,15 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     knowledge_base_id = args.knowledge_base_id
-    prompt = args.prompt
+    prompt = "Answer the following question:\n\n" + args.prompt + "\n\nYour response should not include additional actions for the user to fulfil"
     thread_id = args.thread_id
     ruleset_alias = args.ruleset_alias
     stream = args.stream
 
     Defaults.drivers_config.conversation_memory_driver = GriptapeCloudConversationMemoryDriver(
         thread_id=thread_id,
+        base_url=os.environ["GT_CLOUD_BASE_URL"],
+        api_key=os.environ["GT_CLOUD_API_KEY"],
     )
 
     pipeline = Pipeline(
@@ -71,10 +78,13 @@ if __name__ == "__main__":
         tasks=[
             AssistantTask(
                 assistant_driver=GriptapeCloudAssistantDriver(
+                    base_url=os.environ["GT_CLOUD_BASE_URL"],
                     assistant_id=os.environ["GT_CLOUD_ASSISTANT_ID"],
                     api_key=os.environ["GT_CLOUD_API_KEY"],
                     stream=False,
+                    max_attempts=1,
                     thread_id=thread_id,
+                    additional_ruleset_ids = [os.environ["RULES"],],
                 ),
             ),
         ]
